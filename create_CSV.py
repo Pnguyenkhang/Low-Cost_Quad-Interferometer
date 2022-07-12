d2c657d8bc025bc6395788c8e3ec737e7e64b2c5
TannerM22 <68660521+TannerM22@users.noreply.github.com>
2022-07-12 13:14:02 -0700

    Update create_CSV.py

--- create_CSV.py
+++ create_CSV.py
@@ -20,9 +20,9 @@ TEMP_value = 0
 t = 0
 init = True
 total_expansion = 0
-PD1_mean =1.59 #manual
-PD2_mean = 0.74 #manual
-sum_phase = 0 # Total phase change
+PD1_mean =1.59   # manual input before run
+PD2_mean = 0.74  # manual input before run
+sum_phase = 0    # Total phase change
 
 K = (2*np.pi)/(532*10**(-9))   #wavenumber depending on wavelength of laser
 
@@ -33,8 +33,9 @@ with open('data.csv', 'w') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     csv_writer.writeheader()
     
-# Connect to trinket through our USB port
 # For windows, will have to find com port connection
+# Connect to trinket through USB port, easiest way to find serial connection is using Adafruit Mu editor and hovering over CPU symbol on bottom right
+# copy and paste USB path name into next line ***with serial.Serial('PATH NAME HERE', 19200, timeout=0.01) as ser:***
 with serial.Serial('/dev/cu.usbmodem14101', 19200, timeout=0.01) as ser:
     while True:
             ct = datetime.datetime.now()
@@ -44,7 +45,7 @@ with serial.Serial('/dev/cu.usbmodem14101', 19200, timeout=0.01) as ser:
             
             line_list = line.split()   #make string into list
             map_object = map(float,line_list)
-            values = list(map_object)
+            values = list(map_object)  # values is a float valued list of incoming data from USB
     
             if init == True:   #initialize expansion list and set prev_theta
                 prev_theta = math.atan2(values[1] - PD2_mean,values[0] - PD1_mean)
@@ -54,7 +55,7 @@ with serial.Serial('/dev/cu.usbmodem14101', 19200, timeout=0.01) as ser:
                 init = False
                     
             else:   #calculate change in phase and translate into distance flux
-                if len(values) != 3:
+                if len(values) != 3:   #error checking
                     continue
                 else:
                     PD1_value = values[0]
@@ -63,14 +64,15 @@ with serial.Serial('/dev/cu.usbmodem14101', 19200, timeout=0.01) as ser:
                     
                     theta_value = math.atan2(values[1] - PD2_mean,values[0] - PD1_mean)
                     delta_theta = theta_value - prev_theta
-
+                    
+                    #due to arctan2() we must check direction of phase change at theta = pi or theta = -pi
                     if delta_theta > 6:
                         delta_theta = delta_theta - 2*np.pi
                     elif delta_theta < -6:
                         delta_theta = delta_theta +2*np.pi
                     prev_theta = theta_value
                     sum_phase += delta_theta
-                    if sum_phase > 2*math.pi or sum_phase < -2*math.pi: # If 360 phase angle reached, reset total
+                    if sum_phase > 2*math.pi or sum_phase < -2*math.pi:   # If 2*pi phase angle reached, reset total
                         sum_phase = 0
                         data_2 = pd.read_csv('data.csv')
                         stream_1 = data_2['PD1']
@@ -79,10 +81,12 @@ with serial.Serial('/dev/cu.usbmodem14101', 19200, timeout=0.01) as ser:
                         PD2_mean = (np.max(stream_2[-index:]) +np.min(stream_2[-index:]))/2
                         index = 0
                         theta_value = math.atan2(values[1] - PD2_mean,values[0] - PD1_mean)
-            
+                        
+                #convert phase change to expansion
                 expansion_step = delta_theta/K
                 total_expansion += expansion_step
-
+                
+                #log all data into data.csv
                 with open('data.csv', 'a') as csv_file:
                     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
